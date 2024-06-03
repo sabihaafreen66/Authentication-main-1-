fb347e9a94ebdc54e1fe9a4dcce8a6ae656ba0c4
SabihaAfreen <afrah9059@gmail.com>
2024-06-03 14:08:14 +0530

    n

--- Authentication-main/src/App.js
+++ Authentication-main/src/App.js
@@ -1,38 +1,58 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
-import { useAuth } from './store/auth-context';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
 
-function App() {
-  const authCtx = useAuth();
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  const checkToken = () => {
+    const token = localStorage.getItem('userToken');
+    const expirationTime = localStorage.getItem('tokenExpiration');
+    const currentTime = new Date().getTime();
+
+    if (token && expirationTime && currentTime < expirationTime) {
+      setIsAuthenticated(true);
+    } else {
+      logoutUser();
+    }
+  };
+
+  const loginUser = (token) => {
+    const expirationTime = new Date().getTime() + 5 * 60 * 1000; // 5 minutes from now
+    localStorage.setItem('userToken', token);
+    localStorage.setItem('tokenExpiration', expirationTime);
+    setIsAuthenticated(true);
+    setLogoutTimer(5 * 60 * 1000);
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem('userToken');
+    localStorage.removeItem('tokenExpiration');
+    setIsAuthenticated(false);
+    alert('Session has expired. Please log in again.');
+  };
+
+  const setLogoutTimer = (timeout) => {
+    setTimeout(() => {
+      logoutUser();
+    }, timeout);
+  };
 
   return (
-    <Layout>
-      <Switch>
-        {!authCtx.isLoggedIn && (
-          <>
-            <Route path='/auth'>
-              <AuthPage />
-            </Route>
-            <Redirect to='/auth' />
-          </>
-        )}
-        {authCtx.isLoggedIn && (
-          <>
-            <Route path='/' exact>
-              <HomePage />
-            </Route>
-            <Route path='/profile'>
-              <UserProfile />
-            </Route>
-            <Redirect to='/' />
-          </>
-        )}
-      </Switch>
-    </Layout>
+    <Router>
+      <Route exact path="/">
+        {isAuthenticated ? <Home /> : <Redirect to="/login" />}
+      </Route>
+      <Route path="/login">
+        <Login onLogin={loginUser} />
+      </Route>
+    </Router>
   );
-}
+};
 
 export default App;
